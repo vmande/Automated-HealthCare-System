9c00610fab40495afd73499838470c547b99481e
Vaishnavi Mande <mande.v@husky.neu.edu>
2020-04-12 10:30:04 -0400

    Pushing business package

---
+++ src/Business/Organization/HealthCareOfficerOrganization.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.GovernmentHealthOfficerRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+
+public class HealthCareOfficerOrganization extends Organization {
+
+    public HealthCareOfficerOrganization() {
+        super(Organization.Type.HealthcareOfficer.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new GovernmentHealthOfficerRole());
+        return roles;
+    }
+
+}
