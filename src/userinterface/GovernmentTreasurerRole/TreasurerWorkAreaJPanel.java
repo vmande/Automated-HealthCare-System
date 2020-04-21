abbf17b1a6013bd7cd9fc606471e79d6cc05fb7f
Vaishnavi Mande <mande.v@husky.neu.edu>
2020-04-20 22:21:47 -0400

    Healthcare Accountant Table sorter

--- src/userinterface/GovernmentTreasurerRole/TreasurerWorkAreaJPanel.java
+++ src/userinterface/GovernmentTreasurerRole/TreasurerWorkAreaJPanel.java
@@ -15,6 +15,7 @@ import java.awt.CardLayout;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -220,6 +221,8 @@ public class TreasurerWorkAreaJPanel extends javax.swing.JPanel {
 
             model.addRow(row);
         }
+        TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(model);
+        workRequestJTable.setRowSorter(sorter);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
